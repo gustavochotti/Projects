cc3525abb3908ec1d88991aabf988b79bc19f7e4
gustavochotti <106937773+gustavochotti@users.noreply.github.com>
2022-10-04 02:08:09 -0300

    Update weather_fetcher.py

--- weather_fetcher.py
+++ weather_fetcher.py
@@ -2,7 +2,7 @@
 import requests
 
 # paste the API and Base Url:
-API_KEY = "your API key"
+API_KEY = "your API key here"
 BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
 
 # put city name and request data
@@ -10,16 +10,17 @@ city = input("Enter a city name: ")
 requests_url = f"{BASE_URL}?appid={API_KEY}&q={city}"
 response = requests.get(reuqests_url)
 
-# create a condition to run the code
+# create a condition and put the commands
 if response.status_code == 200:
     data = response.json()
-    main_weather = data['weather'][0]['main']
+    main_weather = data['weather'][0]['main'] 
     description_weather = data['weather'][0]['description']
     temperature = round(data['main']['temp']-273.15, 0)
-    temperature_maximum = round(data['main']['temp_max']-273.15, 0)
+    temperature_maximum = round(data['main']['temp_max']- 273.15, 0) # -273.15 to convert from Kelvin to Celsius
     temperature_minimum = round(data['main']['temp_min'] - 273.15, 0)
     temperature_feels = round(data['main']['feels_like'] - 273.15, 0)
 
+    # show the weather
     print(main_weather, '-', description_weather)
     print('temperature:', temperature)
     print(f'Max: {temperature_maximum}c / Min: {temperature_minimum}c - Feels Like: {temperature_feels}c')
